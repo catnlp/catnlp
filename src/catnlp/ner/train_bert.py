9fbfb41dc6bcc6e1dd2dc93d03719ea4d9d59ac2
cat <catnlp@163.com>
2021-05-08 22:55:12 +0800

    跑通bert流程

--- src/catnlp/ner/train_bert.py
+++ src/catnlp/ner/train_bert.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
-# coding=utf-8
+# -*- coding: utf-8 -*-
+
 # Copyright 2021 The HuggingFace Inc. team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
