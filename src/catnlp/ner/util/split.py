7c6e86751f1dbd5f5fcf0651a77666d18e69d24c
cat <catnlp@163.com>
2021-05-30 21:26:25 +0800

    添加split函数

--- src/catnlp/ner/util/split.py
+++ src/catnlp/ner/util/split.py
@@ -1,23 +1,19 @@
 import re
 
 
-def cut(line, max_len, overlap_len):
-    print(line)
-    texts = re.split(r'([。？?，,；;！!]|(?<!\d)\.(?!\d))', line)
-    texts_len = len(texts)
-    print('split:', texts)
+def cut(text, tags, max_len, overlap_len):
+    sents = re.split(r'([。？?，,；;！!]|(?<!\d)\.(?!\d))', text)
+    sents_len = len(sents)
     end_idx_list = list()
-    texts_len = len(texts)
     i = 0
     end_idx = 0
-    result = list()
-    while i < texts_len:
-        print(i)
-        text = texts[i]
-        text_len = len(text)
-        end_idx += text_len
-        print(end_idx)
-        if re.match(r'([。？?，,；;！!]|(?<!\d)\.(?!\d))', text):
+    sent_list = list()
+    tag_lists = list()
+    while i < sents_len:
+        sent = sents[i]
+        sent_len = len(sent)
+        end_idx += sent_len
+        if not sent or re.match(r'([。？?，,；;！!]|(?<!\d)\.(?!\d))', sent):
             i += 1
             continue
         # 搜索前缀
@@ -25,20 +21,20 @@ def cut(line, max_len, overlap_len):
         pre_list_len = 0
         j = i - 1
         while j >= 0:
-            text_j = texts[j]
-            text_j_len = len(text_j)
-            if pre_list_len + text_j_len < overlap_len:
-                pre_list.append(text_j)
-                pre_list_len += text_j_len
+            sent_j = sents[j]
+            sent_j_len = len(sent_j)
+            if pre_list_len + sent_j_len < overlap_len:
+                pre_list.append(sent_j)
+                pre_list_len += sent_j_len
             else:
                 break
             j -= 1
         pre_idx = 0
         pre_list = pre_list[::-1]
-        for tmp_text in pre_list:
-            if re.match(r'([。？?，,；;！!]|(?<!\d)\.(?!\d))', tmp_text):
+        for tmp_sent in pre_list:
+            if not tmp_sent or re.match(r'([。？?，,；;！!]|(?<!\d)\.(?!\d))', tmp_sent):
                 pre_idx += 1
-                pre_list_len -= len(tmp_text)
+                pre_list_len -= len(tmp_sent)
             else:
                 break
         pre_list = pre_list[pre_idx:]
@@ -47,135 +43,58 @@ def cut(line, max_len, overlap_len):
         post_list = list()
         post_list_len = 0
         j = i + 1
-        while j < texts_len:
-            text_j = texts[j]
-            text_j_len = len(text_j)
-            if pre_list_len + text_len + post_list_len < max_len:
-                post_list.append(text_j)
-                post_list_len += text_j_len
+        while j < sents_len:
+            sent_j = sents[j]
+            sent_j_len = len(sent_j)
+            if pre_list_len + sent_len + post_list_len < max_len:
+                post_list.append(sent_j)
+                post_list_len += sent_j_len
             else:
                 break
             j += 1
         
         # 拼接
-        print('pre:', pre_list)
-        print('text', text)
-        print('post:', post_list)
-        result.append("".join(pre_list + [text] + post_list))
+        sent = "".join(pre_list + [sent] + post_list)
+        sent_list.append(sent)
         end_idx += post_list_len
         end_idx_list.append(end_idx)
+        start_idx = end_idx - len(sent)
+        tag_list = tags[start_idx: end_idx]
+        tag_lists.append(tag_list)
         i = j
 
+    if valid(text, sent_list, end_idx_list):
+        return sent_list, tag_lists, end_idx_list
+    else:
+        raise ValueError
 
-    print('result: ', result)
-    print('end: ', end_idx_list)
-    print('test')
-    for end, text in zip(end_idx_list, result):
-        text_len = len(text)
-        start = end - text_len
-        if line[start: end] != text:
+
+def recover(sent_list, entity_lists, end_idx_list):
+    new_entity_list = list()
+    for sent, entity_list, end_idx in zip(sent_list, entity_lists, end_idx_list):
+        offset = end_idx - len(sent)
+        for entity in entity_list:
+            start, end, tag = entity
+            new_entity_list.append([start+offset, end+offset, tag])
+    return new_entity_list
+
+
+def valid(text, sent_list, end_idx_list):
+    for end, sent in zip(end_idx_list, sent_list):
+        sent_len = len(sent)
+        start = end - sent_len
+        if text[start: end] != sent:
             print("---")
-            print(line[start: end])
-            print(text)
-
-# def cut(line, max_len, overlap_len):
-#     print(line)
-#     in_list = list()
-#     texts = re.split(r'([。？?，,；;！!]|(?<!\d)\.(?!\d))', line)
-#     print('split:', texts)
-#     start_idx_list = list()
-#     start_idx = 0
-#     count_in = 0
-#     texts_len = len(texts)
-#     i = 0
-#     end_idx = 0
-#     result = list()
-#     for text in texts:
-#         print(f'inlist: {in_list}')
-#         print(f"{i}/{texts_len}")
-#         i += 1
-#         text_len = len(text)
-#         end_idx += text_len
-#         if count_in == 0 and re.match(r'([。？?，,；;！!]|(?<!\d)\.(?!\d))', text):
-#             continue
-#         if count_in + text_len < max_len:
-#             in_list.append(text)
-#             count_in += text_len
-#         elif count_in + text_len >= max_len:
-#             print("--------")
-#             # tmp_idx = 0
-#             # for tmp_text in in_list:
-#             #     if re.match(r'([。？?，,；;！!]|(?<!\d)\.(?!\d))', tmp_text):
-#             #         tmp_idx += 1
-#             #         count_in -= len(tmp_text)
-#             #     else:
-#             #         break
-#             # in_list = in_list[tmp_idx:]
-#             print(in_list)
-#             if in_list:
-#                 sent = "".join(in_list)
-#                 result.append(sent)
-#                 start_idx = end_idx - text_len - len(sent)
-#                 start_idx_list.append(start_idx)
-            
-#             if re.match(r'([。？?，,；;！!]|(?<!\d)\.(?!\d))', text):
-#                 tmp_idx = 0
-#                 count_in -= len(in_list[0])
-#                 for tmp_text in in_list[1:]:
-#                     if re.match(r'([。？?，,；;！!]|(?<!\d)\.(?!\d))', tmp_text):
-#                         tmp_idx += 1
-#                         count_in -= len(tmp_text)
-#                     else:
-#                         break
-#                 in_list = in_list[tmp_idx+1:]
-#                 if in_list:
-#                     in_list.append(text)
-#                     count_in += len(text)
-#                 print('continue', in_list)
-#                 continue
-
-
-#             count_tmp = 0
-#             tmp_list = [text]
-#             while in_list:
-#                 tmp_text = in_list.pop()
-#                 if count_tmp < overlap_len and text_len + count_tmp < max_len:
-#                     tmp_list.append(tmp_text)
-#                     count_tmp += len(tmp_text)
-#                 else:
-#                     in_list = list()
-            
-#             tmp_idx = len(tmp_list)
-#             print('tmp_list: ', tmp_list)
-#             for tmp_text in tmp_list[::-1]:
-#                 if re.match(r'([。？?，,；;！!]|(?<!\d)\.(?!\d))', tmp_text):
-#                     tmp_idx -= 1
-#                 else:
-#                     break
-#             tmp_list = tmp_list[: tmp_idx]
-#             print('tmp_list: ', tmp_list)
-#             count_in = 0
-#             while tmp_list:
-#                 tmp_text = tmp_list.pop()
-#                 in_list.append(tmp_text)
-#                 count_in += len(tmp_text)
-#             print(in_list)
-#             print("+++++++++++++++++")
-#     if in_list:
-#         sent = "".join(in_list)
-#         result.append(sent)
-#         start_idx = end_idx - len(sent)
-#         start_idx_list.append(start_idx)
-#     print('result: ', result)
-#     print('start: ', start_idx_list)
-#     print('test')
-#     for start, text in zip(start_idx_list, result):
-#         text_len = len(text)
-#         end = start + text_len
-#         if line[start: end] != text:
-#             print(start, text)
+            print(text[start: end])
+            print(sent)
+            return False
+    return True
 
 
 if __name__ == "__main__":
-    text = "sd,fsdfdsfdsfdsfdfdsfk,js,sd,kfj,sd,fkdsf,sdfk,sld,jfk,ds,sd,kfs,jdk,f,dfd,dsfs,sdfsdf,sdfsdf"
-    cut(text, 20, 8)
+    text = "，，s.d,fsdfdsf.dsfdsfdfd,...s.,fk,js,sd,kfj,sd,f,，，k,d,sf,sdfk,sl,d,jfk,ds,s。d。k,，，fs,jdk,f,dfd,dsfs,sdfsdf,sdfsdf"
+    tag_list = ["O"] * len(text)
+    sent_list, tag_lists, end_idx_list = cut(text, tag_list, 20, 8)
+    print(sent_list)
+    print(tag_lists)
+    print(end_idx_list)
