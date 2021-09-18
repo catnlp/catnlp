975f3ef89589525f09ca12a6480ec96b60f8c59b
cat <catnlp@163.com>
2021-09-19 00:12:22 +0800

    添加混淆矩阵显示

---
+++ src/catnlp/tool/draw.py
@@ -0,0 +1,38 @@
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# 绘制混淆矩阵
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Reds):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    Input
+    - cm : 计算出的混淆矩阵的值
+    - classes : 混淆矩阵中每一行每一列对应的列
+    - normalize : True:显示百分比, False:显示个数
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+    print(cm)
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 3.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 verticalalignment="center",
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+    plt.tight_layout()
+    plt.ylabel('Pred label')
+    plt.xlabel('Gold label')
+    plt.show()
---
+++ src/cmeee_confusion.py
@@ -0,0 +1,63 @@
+
+import os
+import json
+import numpy as np
+from catnlp.tool.draw import plot_confusion_matrix
+
+tags = ["dis", "sym", "pro", "equ", "dru", "ite", "bod", "dep", "mic", "oth"]
+tag2idx = {tag: idx for idx, tag in enumerate(tags)}
+matrix = np.zeros((len(tags), len(tags)), dtype=int)
+
+
+def confusion_matrix(gold, pred):
+    with open(gold, "r", encoding="utf-8") as gf, \
+            open(pred, "r", encoding="utf-8") as pf:
+        for gl, pl in zip(gf, pf):
+            gl = json.loads(gl)
+            pl = json.loads(pl)
+            if not gl or not pl:
+                continue
+            g_entities = gl["ner"]
+            p_entities = pl["ner"]
+            g_set = set()
+            for entity in g_entities:
+                g_set.add(tuple(entity))
+            p_set = set()
+            for entity in p_entities:
+                p_set.add(tuple(entity))
+            pos2tag = dict()
+            for entity in g_set:
+                start, end, tag, _ = entity
+                pos2tag[(start, end)] = tag
+            
+            for entity in p_set:
+                start, end, tag, _ = entity
+                row = tag2idx[tag]
+                g_tag = pos2tag.get((start, end), None)
+                if not g_tag:
+                    column = tag2idx["oth"]
+                else:
+                    column = tag2idx[g_tag]
+                matrix[row][column] += 1
+            
+            for entity in p_set:
+                start, end, tag, _ = entity
+                pos2tag[(start, end)] = tag
+            
+            for entity in g_set:
+                start, end, tag, _ = entity
+                column = tag2idx[tag]
+                p_tag = pos2tag.get((start, end), None)
+                if not p_tag:
+                    row = tag2idx["oth"]
+                    matrix[row][column] += 1
+            
+        return matrix
+
+
+if __name__ == "__main__":
+    gold_file = "resources/data/dataset/ner/zh/ccks/cmeee/0808/test_pred_0.json"
+    pred_file = "resources/data/dataset/ner/zh/ccks/cmeee/0814/test_pred_0.json"
+    mat = confusion_matrix(gold_file, pred_file)
+    # plot_confusion_matrix(mat, classes=tags, normalize=True)
+    plot_confusion_matrix(mat, classes=tags, normalize=False)
